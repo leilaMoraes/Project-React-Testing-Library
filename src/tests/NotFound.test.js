2b65fcef492db22bf55c8470e708af25b1bee7b3
Leila Moraes <leila.violino@gmail.com>
2022-10-21 13:10:02 -0300

    conclusão do requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import NotFound from '../pages/NotFound';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testa o componente NotFound', () => {
+  it('1 - Testa se a página contém um heading h2 com o texto `Page requested not found`', () => {
+    renderWithRouter(<NotFound />);
+
+    const titleAbout = screen.getByRole('heading', { level: 2 });
+
+    expect(titleAbout).toHaveTextContent(/page requested not found/i);
+  });
+
+  it('2 - Testa se a página contém a imagem:', () => {
+    renderWithRouter(<NotFound />);
+
+    const img = screen.getByRole('img');
+    const url = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+
+    expect(img).toHaveAttribute('src', url);
+  });
+});
